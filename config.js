e159564ba6ebfd43b163e37d86cf72e068fdac33
CHANG,CHIH-CHING <33929664+ccching@users.noreply.github.com>
2026-05-20 02:05:55 +0800

    v4

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.UNOCHA_CONFIG = {
-  sheetWebAppUrl: "https://script.google.com/a/macros/gs.ncku.edu.tw/s/AKfycbxSJ5ASObvzwScIRMs9_2K28qmbcPrmTrOZrY3a0Z8lfl9eo11bvi2fuq5qm1TWWTe6/exec",
+  sheetWebAppUrl: "https://script.google.com/macros/library/d/1iOrG830YJ2Z_qRH4tvD497FwZP_8Whubx3MkMRgeAqp9nDk9dN6j7_FU/1",
 };
