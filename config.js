7a7613bb55936633f5e68bc6b3aa27cec0e71c8f
CHANG,CHIH-CHING <33929664+ccching@users.noreply.github.com>
2026-05-20 02:19:11 +0800

    Update sheetWebAppUrl in CONFIG

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.UNOCHA_CONFIG = {
-  sheetWebAppUrl: "https://script.google.com/macros/s/AKfycbxSJ5ASObvzwScIRMs9_2K28qmbcPrmTrOZrY3a0Z8lfl9eo11bvi2fuq5qm1TWWTe6/exec",
+  sheetWebAppUrl: "https://script.google.com/macros/s/AKfycbznsppgk_RE6vx82oRpfRZNTVePQR_L9FeX3rHliLathg-6q3TFoTwr5UTInhO1ENos/exec",
 };
