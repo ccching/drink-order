0881a02403f1e78e09733a98deea7ce485937e0d
CHANG,CHIH-CHING <33929664+ccching@users.noreply.github.com>
2026-05-21 23:46:53 +0800

    Update sheetWebAppUrl in config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ window.UNOCHA_CONFIG = {
   activeMenuFile: "menus/93c.js",
   activePollFile: "polls/sandwich.js",
   pageTitle: "NetAI 碩二進度報告5/29",
-  sheetWebAppUrl: "https://script.google.com/macros/s/AKfycbzfZU4ctpTJyYdRzGENscIdJyfXFkr9Ga-9Nuz1dmQKStQv2tJBj8F15XKwf6iM7gIQ/exec",
+  sheetWebAppUrl: "https://script.google.com/macros/s/AKfycbxmK8OHtxiV_43xyHQC6FpQYgwC5iQsjp2K96jEqZldoMDrP1jAGDyynYDu4JOD_Ur1/exec",
   googleSheetUrl: "https://docs.google.com/spreadsheets/d/1ZonRbltBzBWlYMxmASojeLkYEifJCChHezI6twGmBp8/edit?usp=sharing",
 };
